f2b3a446609bb59aba2843771a75c440bd77167c
Simon GRIMAL <simon.grimal@valorem-energie.com>
2018-04-10 12:17:18 +0200

    Ajout fichiers et data pour appli

---
+++ SoccerApp/helpers.R
@@ -0,0 +1,10 @@
+#Scripts sourcé par l'application ou seront stockées les fonctions
+
+#Exemple
+# ExempleDeFonction <- function(Entree1, Entree2){
+#   
+#   #ce que fait ma fonction
+#   
+#   return(SortieDeMaFonction)
+# }
+
